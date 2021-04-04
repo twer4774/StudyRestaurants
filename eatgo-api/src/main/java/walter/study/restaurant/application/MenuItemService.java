3164851dbc23a2992acf7822f5658981cdd3ddcd
wonik <twer4774@gmail.com>
2021-04-04 12:19:48 +0900

    test: write menuitems update

---
+++ eatgo-api/src/main/java/walter/study/restaurant/application/MenuItemService.java
@@ -0,0 +1,14 @@
+package walter.study.restaurant.application;
+
+import org.springframework.stereotype.Service;
+import walter.study.restaurant.domain.MenuItem;
+
+import java.util.List;
+
+@Service
+public class MenuItemService {
+
+    public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems){
+
+    }
+}
